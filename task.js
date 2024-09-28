44a543823e6c65633a85cff4c79ffddd72d1f8eb
Himaahhh <himaah250304@gmail.com>
2024-09-28 12:08:28 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,23 @@
+//function concatstrings
+var a="java"
+var b="script"
+var c=a+b;
+function concat(a,b){
+}
+console.log(c);
+
+//function division
+function division(a,b){
+    return a/b;
+}
+console.log(division(10,5));
+
+//function area
+function area(l,b){
+return area=l*b;
+}
+console.log(area(2,4));
+
+//arrow function
+const double=(a)=>a*2;
+console.log(double(4));
